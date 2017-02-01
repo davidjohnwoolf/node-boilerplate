1b854e814102d65768084f22758c0c9aabc78fca
David Woolf <davidjohnwoolf@gmail.com>
2017-01-31 22:47:58 -0800

    connect to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let app = require('express')();
+let mongoose = require('mongoose');
 let tasks = require('./routes/tasks');
 let port = process.env.PORT || 1349;
+
+//connect to database
+mongoose.connect('mongodb://localhost/node-boilerplate');
 
 //express middleware
 app.use('/', tasks);
