c24cc10b0b42b1c3e24d3c309dfbb29f5bd7b4ee
David Woolf <davidjohnwoolf@gmail.com>
2017-02-02 19:27:18 -0800

    add index.html

--- models/task.js
+++ models/task.js
@@ -2,9 +2,9 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let TaskSchema = new Schema({
-  title: { type: 'string', required: true },
-  description: { type: 'string', required: true },
-  dueDate: { type: 'string', required: true }
+    title: { type: 'string', required: true },
+    description: { type: 'string', required: true },
+    dueDate: { type: 'string', required: true }
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
--- routes/tasks.js
+++ routes/tasks.js
@@ -8,26 +8,26 @@ router.use(bodyParser.json());
 
 //index
 router.get('/task', (req, res) => {
-  Task.find((err, tasks) => {
-    if (err) return res.send(err);
-    res.json(tasks);
-  });
+    Task.find((err, tasks) => {
+        if (err) return res.send(err);
+        res.json(tasks);
+    });
 });
 
 //create
 router.post('/task', (req, res) => {
-  let task = new Task({
-    title: req.body.title,
-    description: req.body.description,
-    dueDate: req.body.dueDate
-  });
+    let task = new Task({
+        title: req.body.title,
+        description: req.body.description,
+        dueDate: req.body.dueDate
+    });
 
-  // save the bear and check for errors
-  task.save((err) => {
-    if (err) res.send(err);
+    // save the bear and check for errors
+    task.save((err) => {
+        if (err) res.send(err);
 
-    res.json({ message: 'Task created!' });
-  });
+        res.json({ message: 'Task created!' });
+    });
 })
 
 module.exports = router;
