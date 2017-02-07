11bf5a89f0bc8fceef03a195763a4884c1ada53d
David Woolf <davidjohnwoolf@gmail.com>
2017-02-06 23:18:43 -0800

    format app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let express = require('express');
-let app = express();
 let mongoose = require('mongoose');
 let index = require('./routes/index');
 let tasks = require('./routes/tasks');
 let port = process.env.PORT || 1349;
+let app = express();
 
 //database
 mongoose.connect('mongodb://localhost/node-boilerplate');
