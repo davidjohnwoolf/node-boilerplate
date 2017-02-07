9b8c6c0dcbe9b9305788ded6cfecf178a87b6a5b
David Woolf <davidjohnwoolf@gmail.com>
2017-02-06 23:17:31 -0800

    add tasks show route

--- routes/tasks.js
+++ routes/tasks.js
@@ -9,7 +9,8 @@ router.use(bodyParser.json());
 //index
 router.get('/task', (req, res) => {
     Task.find((err, tasks) => {
-        if (err) return res.send(err);
+        if (err) return res.json(err);
+
         res.json(tasks);
     });
 });
@@ -24,10 +25,19 @@ router.post('/task', (req, res) => {
 
     // save the bear and check for errors
     task.save((err) => {
-        if (err) res.send(err);
+        if (err) res.json(err);
 
         res.json({ message: 'Task created!' });
     });
-})
+});
+
+//show
+router.get('/task/:id', (req, res) => {
+    Task.findOne({ _id: req.params.id }, (err, task) => {
+        if (err) return res.json(err);
+
+        res.json(task);
+    });
+});
 
 module.exports = router;
