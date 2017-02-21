7d9df5f6ab8ea6e69dafed211a96ae2b2686ac66
David Woolf <davidjohnwoolf@gmail.com>
2017-02-20 20:07:34 -0800

    set up tests

---
+++ test/task.js
@@ -0,0 +1,12 @@
+let mongoose = require('mongoose');
+let Task = require('../models/task');
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let app = require('../app');
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('tasks', () => {
+    //tests
+});
