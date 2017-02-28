dc0c2f0e184387d7799c9ad3bce9c25107d49708
David Woolf <davidjohnwoolf@gmail.com>
2017-02-27 20:27:47 -0800

    add get index test

--- test/task.js
+++ test/task.js
@@ -13,4 +13,17 @@ describe('tasks', () => {
            done();
         });
     });
+
+    describe('get task', () => {
+        it('should get all the books', (done) => {
+            chai.request(app)
+                .get('/task')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    res.body.length.should.be.eql(0);
+                    done()
+                });
+        });
+    });
 });
